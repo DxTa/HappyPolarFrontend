8fcd63ed7f0ac3fd026a090f331160e9637ede68
Minh Duc Ta <ductm310@gmail.com>
2016-03-19 14:20:57 +0200

    first commit

---
+++ containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ProductsContainer from './ProductsContainer'
+import CartContainer from './CartContainer'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Happy Polar</h2>
+        <hr/>
+      </div>
+    )
+  }
+}
